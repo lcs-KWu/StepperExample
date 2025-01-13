cab47c5ed5c8ca19ad9ca53e798db1d80dddc94c
Kevin Wu <KWu@lcs.on.ca>
2025-01-13 09:19:39 -0500

    Added squared int and fixed the math error

--- StepperExample/ContentView.swift
+++ StepperExample/ContentView.swift
@@ -13,20 +13,23 @@ struct ContentView: View {
     @State var base: Int = 1
     
     // MARK: Computed properties
+    var squared : Int {
+        return base * base }
     var body: some View {
         VStack {
             Spacer()
             HStack(alignment: .top) {
-                Text("5")
-                Text("\(base)")
+                Text("(\(base))")
                     .font(.system(size: 96))
                 Text("2")
                     .font(.system(size: 44))
                 Text("=")
                     .font(.system(size: 96))
-                Text("25")
+                Text("\(squared)")
                     .font(.system(size: 96))
             }
+            .lineLimit(1)
+            .minimumScaleFactor(0.5)
             Stepper(value: $base, label: {
                 Text("Base")
             })
